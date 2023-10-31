5eeb24898d82a6ba544e29d596e69d3bc726650b
Mark Anthony <markanthonykariukikamau@gmail.com>
2023-10-31 08:21:13 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -39,5 +39,18 @@ char *str_concat(char *s1, char *s2)
 		conct[i] = s1[i];
 		i++;
 	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[i] = s2[ci];
+
+		i++, ci++;
+
+	}
+
+	conct[i] = '\0';
+
+	return (conct);
+
 }
 
